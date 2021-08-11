074d04fc5088f91db9aab91ceb8ce6399b6463da
VarunM <VarunM@hollywoodbets.net>
2021-08-11 07:42:53 +0200

    chore: plugins import for prod env, correct public Path for prod GitHub pages deploy

--- webpack/prod.webpack.js
+++ webpack/prod.webpack.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 const { resolve } = require('path');
 const { mergeWithCustomize } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const InlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const commonClientConfig = require('./common.webpack');
 
 module.exports = mergeWithCustomize({
@@ -19,7 +22,7 @@ module.exports = mergeWithCustomize({
         path: resolve(__dirname, '../dist'),
         chunkFilename: '[name].js',
         filename: 'js/[name].min.js',
-        publicPath: '',
+        publicPath: '/LearningReactApp/',
     },
     module: {
         rules: [],
@@ -36,6 +39,27 @@ module.exports = mergeWithCustomize({
           columns: true,
           test: /\.js?$/,
           filename: 'js/[name].[chunkhash].js.map',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: 'static/config.js',
+                    to: 'config/config.js',
+                }
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            template: 'static/index.html',
+            inlineSource: 'runtime~.+\\.js',
+            chunksSortMode: 'none',
+            favicon: 'static/favicon.ico',
+        }),
+        new InlineSourcePlugin(HtmlWebpackPlugin),
+        new webpack.DefinePlugin({
+            'process.env': {
+              REACT_APP_LANG: process.env.REACT_APP_LANG ? `"${process.env.REACT_APP_LANG}"` : '"en"', // Use the env var set at build time
+            },
         }),
     ],
 }
